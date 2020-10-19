64a01b612850af10aa4060493da39dffc86b2220
CalebAnyaeche <anyaechecaleb00@gmail.com>
2020-10-19 16:14:05 -0500

    Add timestamp to frame

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -80,5 +80,13 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
                 (x, y, w, h) = cv2.boundingRect(c)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 text = "Occupied"
+
+        # draw the text and timestamp on the frame
+        ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
+
  
 
